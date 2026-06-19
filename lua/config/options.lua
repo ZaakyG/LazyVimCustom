51064f1b43826a905348d45e66f21f9daff62a91
ZaakyG <urisac.gc@hotmail.com>
2026-06-19 14:10:04 -0600

    Some settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,6 +7,14 @@ vim.g.snacks_animate = false
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
+--vim.opt.guicursor = {
+--  "n-v-c:block",   -- normal mode: block
+--  "i-ci-ve:ver25", -- insert mode: vertical line (25% width)
+--  "r-cr:hor20",    -- replace mode: horizontal
+--  "o:hor50",
+--}
+vim.opt.guicursor = "n-v-c:block,i:ver25-blinkwait300-blinkon200-blinkoff150"
+
 local function set_background_from_os()
   local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
   local result = ""
@@ -16,10 +24,23 @@ local function set_background_from_os()
   end
 
   if result:match("Dark") then
-    vim.cmd("colorscheme tokyonight")
+    vim.cmd("colorscheme miniautumn")
   else
+    --vim.cmd("colorscheme miniautumn")
+    -- vim.opt.background = "light"
+    vim.cmd("colorscheme tokyonight-day")
+
+    vim.schedule(function()
+      vim.opt.guicursor = {
+        "n-v-c:block",
+        "i-ci-ve:ver30-blinkwait150-blinkon200-blinkoff150",
+        "r-cr:hor20"
+      }
+      vim.api.nvim_set_hl(0, "Cursor", { fg = "white", bg = "black" })
+      vim.api.nvim_set_hl(0, "TermCursor", { fg = "white", bg = "black" })
+    end)
+
     --vim.cmd("colorscheme tokyonight-day")
-    --vim.opt.background = "light"
 
     --vim.api.nvim_set_hl(0, "LineNr", { fg = "#808080" })
     --    vim.api.nvim_set_hl(0, "CursorLineNr", {
@@ -27,10 +48,10 @@ local function set_background_from_os()
     --      fg = "#ffffff",
     --      bg = "#000000",
     --    })
-    -- vim.api.nvim_set_hl(0, "CursorLine", {
-    --   bg = "#000000",
-    --   fg = "#ffffff",
-    -- })
+    --vim.api.nvim_set_hl(0, "Cursor", {
+    --  bg = "gray",
+    --  fg = "black",
+    --})
   end
 end
 
